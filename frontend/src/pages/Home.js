291dfde8dd582f93f567347d06de218c724e5985
josephfernicola <josephrfernicola@gmail.com>
2022-08-21 20:45:16 -0700

    added homepage text

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-
 const Home = () => {
   return (
     <div className="homeContainer">
       <div className="background-image"></div>
       <div className="homeContent">
         <p className="homepageWelcome"> Noble Electronic Health Record</p>
-        <p className="homepageText"></p>
+        <p className="homepageText">
+          Create notes for your patients, edit your notes, view other provider
+          notes
+        </p>
       </div>
     </div>
   );
