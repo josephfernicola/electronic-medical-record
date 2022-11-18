6e8b777c6f1594f43f1bfc3964d9dfa8d772fdc4
josephfernicola <josephrfernicola@gmail.com>
2022-11-18 06:50:31 -0800

    changed login ehr to emr

--- frontend/src/pages/LoginScreen.js
+++ frontend/src/pages/LoginScreen.js
@@ -18,47 +18,46 @@ const LoginScreen = () => {
       <div className="loginTitleAndFormContainer">
         <div className="loginTitleContainer">
           <div className="loginTitle">
-            Welcome to Noble Electronic Health Record System
+            Welcome to Noble Electronic Medical Record System
           </div>
           <div className="loginDescription">
             Less time spent on documentation, means more time for you to deliver
             quality care
           </div>
         </div>
-        
-          <form className="login" onSubmit={handleSubmit}>
-            <h3>Login</h3>
-            <div>
-              <label htmlFor="email"> Email:</label>
-              <input
-                type="email"
-                name="email"
-                autoComplete="off"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-            </div>
-            <div>
-              <label htmlFor="password"> Password:</label>
-              <input
-                type="password"
-                name="password"
-                autoComplete="off"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-            </div>
 
-            <button
-              className="loginFormButton"
-              type="submit"
-              disabled={isLoading}
-            >
-              Log in
-            </button>
-            {error && <div className="error">{error}</div>}
-          </form>
-   
+        <form className="login" onSubmit={handleSubmit}>
+          <h3>Login</h3>
+          <div>
+            <label htmlFor="email"> Email:</label>
+            <input
+              type="email"
+              name="email"
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
+          <div>
+            <label htmlFor="password"> Password:</label>
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div>
+
+          <button
+            className="loginFormButton"
+            type="submit"
+            disabled={isLoading}
+          >
+            Log in
+          </button>
+          {error && <div className="error">{error}</div>}
+        </form>
       </div>
     </div>
   );
