1f6c72d4b8ba152729261e0397fa97131d487338
josephfernicola <josephrfernicola@gmail.com>
2023-03-12 09:29:05 -0700

    Change homepage layout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,16 @@
+import { FaNotesMedical } from "react-icons/fa";
+
 const Home = () => {
   return (
     <main className="homeContainer">
       <div className="background-image"></div>
       <div className="homeContent">
-        <div className="homepageWelcome"> Noble Electronic Medical Record</div>
+        <div className="homepageWelcome">
+          {<FaNotesMedical />} Noble
+          <div>Electronic Medical Record System</div>
+        </div>
+        <hr />
+
         <div className="homepageText">
           Create notes for your patients, edit your notes, view other provider
           notes
